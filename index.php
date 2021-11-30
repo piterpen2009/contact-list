3933c98738162767d45b77738492a92b03705776
fedyancev.d.a <fedyancev.d.a@effective-group.ru>
2021-12-01 01:10:30 +0300

    Добавил валидацию и логировние, затем передал проект под функции.

--- index.php
+++ index.php
@@ -1,76 +1,142 @@
 <?php
-$pathToRecipient = __DIR__ . '/rec/recipient.json';
-$recipientTxt = file_get_contents($pathToRecipient);
-$recipients = json_decode($recipientTxt,true);
+/**
+ * Функция логирует текстовое сообщение
+ *
+ * @param string $errMsg - сообщение о ошибке
+ */
+function logger ( string $errMsg):void
+{
+    file_put_contents(__DIR__ . '/app.log',"{$errMsg}\n", FILE_APPEND);
+}
+/**
+ *
+ *
+ * @param string $message - сообщение о причине ошибке
+ * @param int $httpCode - http code
+ * @param string $status - статус ошибки
+ */
+function errorHandling(string $message, int $httpCode, string $status): void
+{
+    $result = [
+        'status' => $status,
+        'message' => $message
+    ];
+    logger($message);
+    render($result, $httpCode);
+}
 
-$pathToCustomer = __DIR__ . '/rec/customers.json';
-$customerTxt = file_get_contents($pathToCustomer);
-$customers = json_decode($customerTxt,true);
+/**
+ * Функция вывода данных
+ *
+ * @param array $data - данные, которые хотим отобразить
+ * @param int $httpCode - http code
+ */
+function render(array $data, int $httpCode)
+{
+    header('Content-Type: application/json');
+    http_response_code($httpCode);
+    echo json_encode($data);
+    exit();
+}
 
-$pathToKinsfolk = __DIR__ . '/rec/kinsfolk.json';
-$kinsfolkTxt = file_get_contents($pathToKinsfolk);
-$kinsfolk = json_decode($kinsfolkTxt,true);
+/**
+ * Функция валидации
+ *
+ * @param string $paramName - имя параметра
+ * @param array $param - имя запроса
+ * @param string $errorMessage - сообщение об ошибке
+ */
+function paramTypeValidation(string $paramName, array $param, string $errorMessage)
+{
+    if (array_key_exists($paramName, $param) && false === is_string($param[$paramName])) {
+        errorHandling($errorMessage, 500, 'fail');
+    }
+}
 
-$pathToColleagues = __DIR__ . '/rec/colleagues.json.json';
-$colleaguesTxt = file_get_contents($pathToColleagues);
-$colleagues = json_decode($colleaguesTxt,true);
+/** Функция перводит данные из json формата в php и возвращает содержимое
+ *
+ * @param string $sourceName - имя файла
+ * @return array - содержимое json файла
+ */
+function loadData (string $sourceName):array
+{
+    $pathToFile = __DIR__ . "/rec/{$sourceName}.json";
+    $content = file_get_contents($pathToFile);
+    return json_decode($content, true);
+}
 
-$pathToLogFile = __DIR__.'/app.log';
-
-file_put_contents($pathToLogFile,'Url request received' . $_SERVER['REQUEST_URI'] . "\n", FILE_APPEND);
-
-$pathInfo = array_key_exists('PATH_INFO', $_SERVER) && $_SERVER['PATH_INFO'] ? $_SERVER['PATH_INFO'] : '';
-
-if('/recipient' === $pathInfo) {
+/**
+ * Функция поиска знакомых по id или full_name
+ * @return array - выводит результат поиска по знакомым
+ */
+function findRecipient():array
+{
+    $recipients = loadData('recipient');
     $httpCode = 200;
     $result = [];
-
-    file_put_contents($pathToLogFile,'dispatch "recipient" url' . "\n", FILE_APPEND);
+    logger('dispatch "recipient" url');
+    paramTypeValidation('id_recipient', $_GET, 'incorrect id_recipient');
+    paramTypeValidation('full_name', $_GET, 'incorrect full_name');
 
     $recipientIDToInfo = [];
     foreach ($recipients as $recipientInfo) {
         $recipientIDToInfo[$recipientInfo['id_recipient']] = $recipientInfo;
     }
     foreach ($recipients as $recipient) {
-        if(array_key_exists('id_recipient', $_GET)) {
-            $recipientMeetSearchCriteria = $_GET['id_recipient'] == $recipientIDToInfo[$recipient['id_recipient']]['id_recipient'];
+        if (array_key_exists('id_recipient', $_GET)) {
+            $recipientMeetSearchCriteria = $_GET['id_recipient'] == (string)$recipientIDToInfo[$recipient['id_recipient']]['id_recipient'];
         } elseif (array_key_exists('full_name', $_GET)) {
-            $recipientMeetSearchCriteria = $_GET['full_name'] == $recipientIDToInfo[$recipient['id_recipient']]['full_name'];
+            $recipientMeetSearchCriteria = $_GET['full_name'] === $recipientIDToInfo[$recipient['id_recipient']]['full_name'];
         } elseif (array_key_exists('birthday', $_GET)) {
-            $recipientMeetSearchCriteria = $_GET['birthday'] == $recipientIDToInfo[$recipient['id_recipient']]['birthday'];
+            $recipientMeetSearchCriteria = $_GET['birthday'] === $recipientIDToInfo[$recipient['id_recipient']]['birthday'];
         } elseif (array_key_exists('profession', $_GET)) {
-            $recipientMeetSearchCriteria = $_GET['profession'] == $recipientIDToInfo[$recipient['id_recipient']]['profession'];
+            $recipientMeetSearchCriteria = $_GET['profession'] === $recipientIDToInfo[$recipient['id_recipient']]['profession'];
         } else {
             $recipientMeetSearchCriteria = true;
         }
-
         if ($recipientMeetSearchCriteria) {
             $result[] = $recipientIDToInfo[$recipient['id_recipient']];
         }
     }
-    file_put_contents($pathToLogFile,'found recipients not category: '. count($result) . "\n", FILE_APPEND);
-} elseif ('/customers' === $pathInfo) {
-    file_put_contents($pathToLogFile,'dispatch "customers" url' . "\n", FILE_APPEND);
+    logger('found recipients not category: '. count($result));
+    return [
+        'httpCode' => $httpCode,
+        'result' => $result
+    ];
+
+}
+
+/**
+ * Функция поиска клиента по id или full_name
+ * @return array - выводит результат поиска по клиентам
+ */
+function findCustomers():array
+{
+    $customers = loadData('customers');
+    logger('dispatch "customers" url');
     $httpCode = 200;
     $result = [];
 
+    paramTypeValidation('id_recipient', $_GET, 'incorrect id_recipient');
+    paramTypeValidation('full_name', $_GET, 'incorrect full_name');
+
     foreach ($customers as $customer) {
-        if(array_key_exists('id_recipient', $_GET)) {
-            $customerMeetSearchCriteria = $_GET['id_recipient'] == $customer['id_recipient'];
+        if (array_key_exists('id_recipient', $_GET)) {
+            $customerMeetSearchCriteria = $_GET['id_recipient'] === (string)$customer['id_recipient'];
         } elseif (array_key_exists('full_name', $_GET)) {
-            $customerMeetSearchCriteria = $_GET['full_name'] == $customer['full_name'];
+            $customerMeetSearchCriteria = $_GET['full_name'] === $customer['full_name'];
         } elseif (array_key_exists('birthday', $_GET)) {
-            $customerMeetSearchCriteria = $_GET['birthday'] == $customer['birthday'];
+            $customerMeetSearchCriteria = $_GET['birthday'] === $customer['birthday'];
         } elseif (array_key_exists('profession', $_GET)) {
-            $customerMeetSearchCriteria = $_GET['profession'] == $customer['profession'];
+            $customerMeetSearchCriteria = $_GET['profession'] === $customer['profession'];
         } elseif (array_key_exists('contract_number', $_GET)) {
-            $customerMeetSearchCriteria = $_GET['contract_number'] == $customer['contract_number'];
+            $customerMeetSearchCriteria = $_GET['contract_number'] === $customer['contract_number'];
         } elseif (array_key_exists('average_transaction_amount', $_GET)) {
-            $customerMeetSearchCriteria = $_GET['average_transaction_amount'] == $customer['average_transaction_amount'];
+            $customerMeetSearchCriteria = $_GET['average_transaction_amount'] === (string)$customer['average_transaction_amount'];
         } elseif (array_key_exists('discount', $_GET)) {
-            $customerMeetSearchCriteria = $_GET['discount'] == $customer['discount'];
+            $customerMeetSearchCriteria = $_GET['discount'] === $customer['discount'];
         } elseif (array_key_exists('time_to_call', $_GET)) {
-            $customerMeetSearchCriteria = $_GET['time_to_call'] == $customer['time_to_call'];
+            $customerMeetSearchCriteria = $_GET['time_to_call'] === $customer['time_to_call'];
         } else {
             $customerMeetSearchCriteria = true;
         }
@@ -78,41 +144,74 @@ if('/recipient' === $pathInfo) {
             $result[] = $customer;
         }
     }
-    file_put_contents($pathToLogFile,'found customers not category: '. count($result) . "\n", FILE_APPEND);
+    logger('found customers not category: '. count($result) );
+    return [
+        'httpCode' => $httpCode,
+        'result' => $result
+    ];
+}
 
-} elseif ('/category' === $pathInfo) {
+/**
+ * Функция поиска контакттов по категории
+ * @return array - результат поиска по категориям
+ */
+function findContactOnCategory():array
+{
+    $customers = loadData('customers');
+    $recipients = loadData('recipient');
+    $kinsfolk = loadData('kinsfolk');
+    $colleagues = loadData('colleagues');
     $httpCode = 200;
     $result = [];
+    $categoryList = ['customers','kinsfolk','colleagues','recipients'];
+    logger('dispatch "category" url');
     if (array_key_exists('category', $_GET)) {
         if ($_GET['category'] === 'customers') {
-            file_put_contents($pathToLogFile,'dispatch category "customers"' . "\n", FILE_APPEND);
+            logger('dispatch category "customers"');
             $result = $customers;
-            file_put_contents($pathToLogFile,'found customers: '. count($result) . "\n", FILE_APPEND);
+            logger('found customers: '. count($result));
         } elseif ($_GET['category'] === 'recipients') {
-            file_put_contents($pathToLogFile,'dispatch category "recipients"' . "\n", FILE_APPEND);
+            logger('dispatch category "recipients"');
             $result = $recipients;
-            file_put_contents($pathToLogFile,'found recipients: '. count($result) . "\n", FILE_APPEND);
+            logger('found customers: '. count($result));
         } elseif ($_GET['category'] === 'kinsfolk') {
-            file_put_contents($pathToLogFile,'dispatch category "kinsfolk"' . "\n", FILE_APPEND);
+            logger('dispatch category "kinsfolk"');
             $result = $kinsfolk;
-            file_put_contents($pathToLogFile,'found kinsfolk: '. count($result) . "\n", FILE_APPEND);
+            logger('found kinsfolk: '. count($result));
         } elseif ($_GET['category'] === 'colleagues') {
-            file_put_contents($pathToLogFile,'dispatch category "colleagues"' . "\n", FILE_APPEND);
+            logger('dispatch category "colleagues"');
             $result = $colleagues;
-            file_put_contents($pathToLogFile,'found colleagues: '. count($result) . "\n", FILE_APPEND);
+            logger('found colleagues: '. count($result));
+        } else {
+            errorHandling('dispatch category nothing', 500,'fail');
         }
     }
-
-} else {
-    file_put_contents($pathToLogFile, 'unsupported request' ."\n", FILE_APPEND);
-
-    $httpCode=404;
-    $result=[
-        'status'=>'fail',
-        'message'=>'unsupported request'
+    return [
+        'httpCode' => $httpCode,
+        'result' => $result
     ];
-    file_put_contents($pathToLogFile,'error url' . "\n", FILE_APPEND);
 }
-header('Content-Type: application/json');
-http_response_code($httpCode);
-echo json_encode($result);
+
+/** Функция реализации веб приложения
+ *
+ * @return array
+ */
+
+function app ():array
+{
+    logger('Url request received' . $_SERVER['REQUEST_URI']);
+    $pathInfo = array_key_exists('PATH_INFO', $_SERVER) && $_SERVER['PATH_INFO'] ? $_SERVER['PATH_INFO'] : '';
+
+    if('/recipient' === $pathInfo) {
+        $result = findRecipient();
+    } elseif ('/customers' === $pathInfo) {
+        $result = findCustomers();
+    } elseif ('/category' === $pathInfo) {
+        $result = findContactOnCategory();
+    } else {
+        errorHandling('unsupported request', 404,'fail');
+    }
+    return $result;
+}
+$resultApp = app();
+render($resultApp['result'], $resultApp['httpCode']);
